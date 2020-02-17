7ce51615d247756ca4e4a883cb7b2b496499ba5c
Henrykvdb <henrykvdb@hotmail.com>
2020-02-17 22:08:41 +0100

    Change render area

--- core/src/com/dropper/DropperCore.kt
+++ core/src/com/dropper/DropperCore.kt
@@ -8,78 +8,74 @@ import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.utils.TimeUtils
-import kotlin.math.min
 
 class DropperCore : ApplicationAdapter() {
-	private lateinit var cam: OrthographicCamera
-	private lateinit var renderer: ShapeRenderer
-	private lateinit var center: Vector2
+    private lateinit var cam: OrthographicCamera
+    private lateinit var renderer: ShapeRenderer
+    private var aspectRatio = 0f
 
-	override fun create() {
-		cam = OrthographicCamera()
-		center = Vector2(Gdx.graphics.width / 2f, Gdx.graphics.height / 2f)
-		cam.setToOrtho(true, center.x, center.y)
-		renderer = ShapeRenderer()
-	}
+    override fun create() {
+        aspectRatio = Gdx.graphics.height.toFloat() / Gdx.graphics.width
+        cam = OrthographicCamera()
+        cam.setToOrtho(false, 2f, 2f * aspectRatio)
+        cam.zoom = 0.5f // don't touch, big dumb
+        cam.position.x = 0f
+        cam.position.y = 0f
+        cam.update()
+        renderer = ShapeRenderer()
+    }
 
-	override fun render() {
-		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1f)
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-		Gdx.gl.glLineWidth(5f)
+    override fun render() {
+        Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        Gdx.gl.glLineWidth(5f)
 
-		val frontRadius = min(Gdx.graphics.height, Gdx.graphics.width) / 2
-		val touch = Vector2(Gdx.input.x.toFloat(), Gdx.input.y.toFloat())
+        //Update camera
+        val inX = -1f + 2 * Gdx.input.x.toFloat() / Gdx.graphics.width
+        val inY = (1f - 2 * Gdx.input.y.toFloat() / Gdx.graphics.height) * aspectRatio
+        val input = Vector2(inX,inY).clamp(0f,1f)
+        cam.position.x = input.x*cam.zoom
+        cam.position.y = input.y*cam.zoom
+        cam.update()
+        renderer.projectionMatrix = cam.combined
 
-		//Clip touch
-		val clippedTouch = touch.sub(center)
-		if (clippedTouch.len() > frontRadius)
-			clippedTouch.scl(frontRadius / clippedTouch.len())
-		clippedTouch.add(center)
+        //Front circle
+        renderer.begin(ShapeRenderer.ShapeType.Filled)
+        renderer.color = Color.GREEN
+        renderer.circle(0f, 0f, 1f)
+        renderer.end()
 
-		//Update camera position
-		val percentX = clippedTouch.x / Gdx.graphics.width
-		val percentY = clippedTouch.y / Gdx.graphics.height
-		cam.position.x = Gdx.graphics.width / 2f + cam.viewportWidth * (percentX - 0.5f)
-		cam.position.y = Gdx.graphics.height / 2f + cam.viewportHeight * (percentY - 0.5f)
-		cam.update()
-		renderer.projectionMatrix = cam.combined
+        //Position dot
+        renderer.begin(ShapeRenderer.ShapeType.Filled)
+        renderer.color = Color.RED
+        renderer.circle(input.x, input.y, 0.2f)
+        renderer.end()
 
-		//Front circle
-		renderer.begin(ShapeRenderer.ShapeType.Filled)
-		renderer.color = Color.GREEN
-		renderer.circle(center.x, center.y, frontRadius.toFloat())
-		renderer.end()
+        //Rings
+        if (TimeUtils.nanoTime() - lastSpawnTime > 1500000000) spawnRing()
 
-		//Position dot
-		renderer.begin(ShapeRenderer.ShapeType.Filled)
-		renderer.color = Color.BLACK
-		renderer.circle(clippedTouch.x, clippedTouch.y, 4f)
-		renderer.end()
+        for (ring in rings) {
+            renderer.begin(ShapeRenderer.ShapeType.Line)
+            renderer.color = Color.BLUE
+            renderer.circle(0f, 0f, ring)
+            renderer.end()
+        }
 
-		//Rings
-		if(TimeUtils.nanoTime() - lastSpawnTime > 1000000000) spawnRing()
+        rings = (rings.map { it + Gdx.graphics.deltaTime })
+                .filter { it <= 1 }.toMutableList()
+        //TODO filter -> collision check & filter
 
-		for (ring in rings) {
-			renderer.begin(ShapeRenderer.ShapeType.Line)
-			renderer.color = Color.BLUE
-			renderer.circle(Gdx.graphics.width / 2.toFloat(), Gdx.graphics.height / 2.toFloat(), ring)
-			renderer.end()
-		}
+    }
 
-		rings = (rings.map { it + 100f*Gdx.graphics.deltaTime })
-				.filter { it<=frontRadius }.toMutableList()
-		//TODO filter -> collision check & filter
-	}
+    private var rings = mutableListOf<Float>()
+    private var lastSpawnTime = 0L
 
-	private var rings = mutableListOf<Float>()
-	private var lastSpawnTime = 0L
+    private fun spawnRing() {
+        rings.add(0.1f)
+        lastSpawnTime = TimeUtils.nanoTime()
+    }
 
-	private fun spawnRing() {
-		rings.add(20f)
-		lastSpawnTime = TimeUtils.nanoTime()
-	}
-
-	override fun dispose() {
-		renderer.dispose()
-	}
+    override fun dispose() {
+        renderer.dispose()
+    }
 }
