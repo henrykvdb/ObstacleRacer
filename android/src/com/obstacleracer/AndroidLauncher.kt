c8fa60c846f5d1aaf921bd5bc5a7addc2a713c42
Henrykvdb <henrykvdb@hotmail.com>
2020-02-29 03:54:32 +0100

    Add firebase

--- android/src/com/obstacleracer/AndroidLauncher.kt
+++ android/src/com/obstacleracer/AndroidLauncher.kt
@@ -1,7 +1,6 @@
 package com.obstacleracer
 
 import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
